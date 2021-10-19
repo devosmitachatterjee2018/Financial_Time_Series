7ec137a42309c0cb5570c1e612839c2ad09bc57a
Devosmita Chatterjee <49337890+devosmitachatterjee2018@users.noreply.github.com>
2021-10-19 16:23:55 +0530

    Update convert_rmd_to_r.R

--- convert_rmd_to_r.R
+++ convert_rmd_to_r.R
@@ -1,5 +1,4 @@
 library(knitr)
-#purl("D:\\Personal\\Courses\\TMS088\\Project1\\Assignment1.Rmd", output = "D:\\Personal\\Courses\\TMS088\\Project1\\Assignment1.R", documentation = 2)
-
+purl("D:\\Personal\\Courses\\TMS088\\Project1\\Assignment1.Rmd", output = "D:\\Personal\\Courses\\TMS088\\Project1\\Assignment1.R", documentation = 2)
 purl("D:\\Personal\\Courses\\TMS088\\Project2\\Assignment2.Rmd", output = "D:\\Personal\\Courses\\TMS088\\Project2\\Assignment2.R", documentation = 2)
 
